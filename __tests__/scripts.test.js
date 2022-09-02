aa3d73c7bb7c1333af7c956038cd2cf3ed2e1bed
Keisha Hepner <kmhepner90@gmail.com>
2022-09-02 15:44:27 -0700

    Good Fail of test('should return mercuryAge based on earthYear 32'

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -1,10 +1,10 @@
 import {Spaceage} from './../src/scripts.js'
 
 describe('Spaceage', () => {
+  
   const earthYears = 32
-  let mercuryAge = new this.mercuryAge();
-  mercuryAge = earthYears * .24;
-  test('should return mercuryAge', () => {
+  let mercuryAge;
+  test('should return mercuryAge based on earthYear 32', () => {
     expect(mercuryAge).toEqual(7.68);
   });
 });
