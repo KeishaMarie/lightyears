5caa7141e86aef8f18523cbf3a2c3d9ad3b1fd0d
Keisha Hepner <kmhepner90@gmail.com>
2022-09-05 20:19:58 -0700

    Confirmed first test is a good fail

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -5,7 +5,7 @@ describe('Spaceage', () => {
   const earthYearsInput = 32
   let mercuryAge;
   test('should return mercuryAge based on earthYear 32', () => {
-    expect(planetAge()).toEqual(7.68);
+    expect(mercuryAge).toEqual(7.68);
   });
 
 
